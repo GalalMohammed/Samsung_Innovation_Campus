7638669bcb81308f67a4e3e1012c1476d3fe5b2b
Galal Mohammed <galalmoh2001@gmail.com>
2023-09-08 11:10:17 +0200

    Transfer to UDP connection for now

--- IoT501/Network_Communication_and_Network_Programming/webapp/echo_client.py
+++ IoT501/Network_Communication_and_Network_Programming/webapp/echo_client.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     app = Flask(__name__)
 
     # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Connect the socket to the port where the server is listening
     server_address = ('192.168.56.102', 999)
@@ -15,17 +15,16 @@ if __name__ == '__main__':
     @app.route('/')
     def select():
         message = b'SELECT'
-        sock.sendall(message)
-        data = sock.recv(16)
+        sock.sendto(message,server_address)
+        data, server = sock.recvfrom(4096)
         return render_template('index.html', clients=eval(data.decode()))
 
-    @app.route('/insert')
+    @app.route('/insert', methods=['GET', 'POST'])
     def insert():
         if request.method == 'POST':
             name = request.form['name']
             message = 'INSERT ' + name
-            message.encode()
-            sock.sendall(message)
+            sock.sendto(message.encode(), server_address)
             return redirect('/')
         return render_template('insert.html')
 
